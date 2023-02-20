aba1ecf6fc85543585c185bd772e71c48c5811c2
Andrey Sushkov <>
2023-02-20 19:50:30 +0300

    Initial commit

---
+++ HTML parsing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTML parsing
+//
+//  Created by Andrei Sushkou on 20.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
